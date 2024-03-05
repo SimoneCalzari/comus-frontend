83adcca7e60f532860acc8e1dd628ee3a026eb5c
SimoneCalzari <calzarisimone@gmail.com>
2024-03-05 19:14:55 +0100

    fix parentesi

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,17 +1,13 @@
 <script>
-
-import AppJumbotron from '../components/AppJumbotron.vue';
-export default{
-    name: 'Home',
-    components: {
-    AppJumbotron
-    }
-
-}
-
-
+import AppJumbotron from "../components/AppJumbotron.vue";
+export default {
+  name: "Home",
+  components: {
+    AppJumbotron,
+  },
+};
 </script>
 
 <template>
- <AppJumbotron />
+  <!-- <AppJumbotron /> -->
 </template>
