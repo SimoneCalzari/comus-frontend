bdc67f9cc0a13869ca0f823f626665a67e92d91b
elisabichiappini <elisabichiappini@gmail.com>
2024-03-06 10:13:30 +0100

    feat: correction section

--- src/components/AppSectionSlogan.vue
+++ src/components/AppSectionSlogan.vue
@@ -5,28 +5,32 @@
 </script>
 
 <template>
-    <!-- <section id="slogan" class="row container px-0">
-        <div class="col-12">
-            <h2>Assapora il cibo degli Dei!</h2>
-            <img class="img-fluid" src="../../public/img/hamburger.jpg" alt="hamburger.jpg">
-        </div>
-    </section> -->
+    <section class="py-5" id="slogan">
+    <div class="container d-flex justify-content-between">
+      <div class="cont-sx">
+        <h3>Non resisto più</h3>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
-// //importazione file variables
-// @import '../assets/scss/partials/variables.scss';
-// #slogan {
-
-//     div {
-//         height: 100px;
-        
-//         h2 {
-//             top: 5rem;
-//             padding: 0 $size_16;
-//             color: $custom-primary; 
-//             font-size: 40px;
-//         }
-//     }    
-// }
+//importazione file variables
+@import '../assets/scss/partials/variables.scss';
+#slogan {
+    background-image: url(../../public/img/hamburger.jpg);
+    background-size: contain;
+    width: 100vw;
+    height: 50vh;
+    color: $custom-primary;
+          
+        h2 {
+            top: 5rem;
+            padding: 0 $size_16;
+            color: $custom-primary; 
+            font-size: 40px;
+        }
+    }    
 </style>
+
+//to do layouts
