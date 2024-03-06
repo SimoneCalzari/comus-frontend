1673d49c01b6a8866e06a7e9d7ab6c54161d6102
AlbertoMessineo <albevorox@gmail.com>
2024-03-06 16:12:22 +0100

    foto ""sistemate""

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,57 +1,92 @@
 <script>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
-  import AppSearch from "../components/AppJumboSearch.vue";
-  
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+import AppSearch from "../components/AppJumboSearch.vue";
 
-  // Import Swiper styles
-  import 'swiper/css';
+// Import Swiper styles
+import "swiper/css";
 
-  
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-      AppSearch
-    },
-    setup() {
-      return {};
-    },
-  };
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+    AppSearch,
+  },
+  setup() {
+    return {};
+  },
+};
 
 // export default {
 //   name: "Jumbotron",
 // };
-
 </script>
 <template>
-  <h2 class="container mt-5 mb-3">Puoi scegliere tra queste categorie di ristorante:</h2>
-  <AppSearch />
-  <swiper :watchSlidesProgress="true" :slidesPerView="5" class="mySwiper">
-    <swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/fast-food.jpg" draggable="false" /></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/italian.jpg" draggable="false" /></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/sushi.jpg"  draggable="false"/></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/mexican.jpg"  draggable="false"/></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/pizza.jpg"  draggable="false"/></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/veggie.jpg" draggable="false" /></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/chinese.jpg" draggable="false" /></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/icecream.jpg"  draggable="false"/></a></swiper-slide
-    ><swiper-slide
-      ><a href="#"><img src="../../public/img/categories_img/coffee.jpg" draggable="false"
-    /></a></swiper-slide>
-  </swiper>
+  <section class="mb-5 pb-5">
+    <h2 class="container mt-5 mb-3">
+      Puoi scegliere tra queste categorie di ristorante:
+    </h2>
+    <AppSearch />
+    <swiper :watchSlidesProgress="true" :slidesPerView="5" class="mySwiper">
+      <swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/italiano.jpg"
+            draggable="false" /></a></swiper-slide
+      ><swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/sushi.jpg"
+            draggable="false" /></a></swiper-slide
+      ><swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/messicano.jpg"
+            draggable="false" /></a></swiper-slide
+      ><swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/pizza.jpg"
+            draggable="false" /></a></swiper-slide
+      ><swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/pesce.jpg"
+            draggable="false" /></a></swiper-slide
+      ><swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/cinese.jpg"
+            draggable="false" /></a></swiper-slide
+      ><swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/gluten-free.jpg"
+            draggable="false" /></a></swiper-slide
+      ><swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/kebab.jpg"
+            draggable="false" /></a
+      ></swiper-slide>
+      <swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/steakhouse.jpg"
+            draggable="false" /></a
+      ></swiper-slide>
+      <swiper-slide
+        ><a href="#"
+          ><img
+            src="../../public/img/categories_img/fast-food.jpg"
+            draggable="false" /></a
+      ></swiper-slide>
+    </swiper>
+  </section>
 </template>
 <style scoped lang="scss">
-@import '../assets/scss/partials/variables.scss';
-h2{
+@import "../assets/scss/partials/variables.scss";
+h2 {
   color: $custom-secondary;
 }
 
@@ -59,20 +94,25 @@ h2{
   width: 100%;
   padding-top: 50px;
   padding-bottom: 50px;
+  
 }
 
 .swiper-slide {
   background-position: center;
   background-size: cover;
   width: 200px;
-  height: 270px;
+  height: 310px;
+  img{
+    width: 200px;
+    height: 300px;
+  }
 }
 
 .swiper-slide img {
   display: block;
   width: 100%;
 }
-.swiper-slide:hover{
+.swiper-slide:hover {
   border: 5px solid $custom-primary;
   border-radius: 5px;
 }
