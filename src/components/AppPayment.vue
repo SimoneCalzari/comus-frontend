d6e8d855f0d0fe32ddbcb405a2e02daf54a3cfb8
JohnQuimson <john.quimson777@gmail.com>
2024-03-11 11:05:06 +0100

    feat: payment

---
+++ src/components/AppPayment.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  name: 'AppPayment',
+  data() {
+    return {};
+  },
+
+  created() {
+    let button = document.querySelector('#submit-button');
+
+    braintree.dropin.create(
+      {
+        authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+        selector: '#dropin-container',
+        locale: 'it_IT',
+      },
+      function (err, instance) {
+        button.addEventListener('click', function () {
+          instance.requestPaymentMethod(function (err, payload) {
+            // Submit payload.nonce to your server
+          });
+        });
+      }
+    );
+  },
+};
+</script>
+
+<template>
+  <div id="dropin-container"></div>
+  <button id="submit-button" class="button button--small button--green">
+    Acquista
+  </button>
+</template>
+
+<style scoped lang="scss">
+@import '../assets/scss/partials/variables.scss';
+
+.button {
+  cursor: pointer;
+  font-weight: 500;
+  left: 3px;
+  line-height: inherit;
+  position: relative;
+  text-decoration: none;
+  text-align: center;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  display: inline-block;
+}
+
+.button--small {
+  padding: 10px 20px;
+  font-size: 0.875rem;
+}
+
+.button--green {
+  outline: none;
+  background-color: #64d18a;
+  border-color: #64d18a;
+  color: white;
+  transition: all 200ms ease;
+}
+
+.button--green:hover {
+  background-color: #8bdda8;
+  color: white;
+}
+</style>
--- src/pages/AppShopping.vue
+++ src/pages/AppShopping.vue
@@ -1,13 +1,22 @@
 <script>
+import AppPayment from '../components/AppPayment.vue';
+
 export default {
   name: 'AppCart',
   data() {
     return {};
   },
+
+  components: {
+    AppPayment,
+  },
 };
 </script>
 
-<template>Carrello</template>
+<template>
+  Carrello
+  <AppPayment />
+</template>
 
 <style scoped lang="scss">
 @use '../assets/scss/general.scss' as *;
