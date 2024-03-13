9fe886f0a309d22c0e41fc5af7c24bfe4bc5ea23
SimoneCalzari <calzarisimone@gmail.com>
2024-03-13 22:49:16 +0100

    feat: loader pagina singolo ristoratore

--- src/pages/AppRestaurantSingle.vue
+++ src/pages/AppRestaurantSingle.vue
@@ -1,18 +1,20 @@
 <script>
-import axios from 'axios';
-import store from '../store';
-import AppCart from '../components/AppCart.vue';
-import AppDish from '../components/AppDish.vue';
+import axios from "axios";
+import store from "../store";
+import AppCart from "../components/AppCart.vue";
+import AppDish from "../components/AppDish.vue";
+import AppPageLoader from "../components/AppPageLoader.vue";
 
 export default {
-  name: 'AppRestaurantSingle',
-  components: { AppDish, AppCart },
+  name: "AppRestaurantSingle",
+  components: { AppDish, AppCart, AppPageLoader },
   data() {
     return {
       store,
-      restaurant: '',
+      restaurant: "",
       modalOn: false,
-      currentModal: '',
+      currentModal: "",
+      isLoading: true,
     };
   },
   methods: {
@@ -27,7 +29,7 @@ export default {
         .get(
           this.store.api.baseUrl +
             this.store.api.apiUrls.restaurants +
-            '/' +
+            "/" +
             this.$route.params.slug
         )
         .catch((error) => {
@@ -35,6 +37,7 @@ export default {
         })
         .then((response) => {
           this.restaurant = response.data.results;
+          this.isLoading = false;
         });
     },
     removeToCart(dish) {
@@ -52,7 +55,7 @@ export default {
           );
         }
         // Aggiorna il carrello nel local storage
-        localStorage.setItem('dishes', JSON.stringify(this.store.cart));
+        localStorage.setItem("dishes", JSON.stringify(this.store.cart));
       }
     },
     addToCart(dish) {
@@ -78,7 +81,7 @@ export default {
         // pusho la copia piatto nel carrello store
         this.store.cart.push(dishWithQuantity);
         // metto il carrello store TUTTO nel local storage alla key 'dishes'
-        localStorage.setItem('dishes', JSON.stringify(this.store.cart));
+        localStorage.setItem("dishes", JSON.stringify(this.store.cart));
         return;
       }
       // se arrivo qua il carrello non è vuoto e devo verificare se ho già il piatto passato alla funzione nel carrello
@@ -96,14 +99,14 @@ export default {
         // pusho la copia piatto nel carrello store
         this.store.cart.push(dishWithQuantity);
         // metto il carrello store TUTTO nel local storage alla key 'dishes'
-        localStorage.setItem('dishes', JSON.stringify(this.store.cart));
+        localStorage.setItem("dishes", JSON.stringify(this.store.cart));
         return;
       }
       // se il piatto è già nel carrello store devo trovarlo e aumentare la sua quantità di uno
       for (let i = 0; i < this.store.cart.length; i++) {
         if (dish.id === this.store.cart[i].id) {
           this.store.cart[i].quantity++;
-          localStorage.setItem('dishes', JSON.stringify(this.store.cart));
+          localStorage.setItem("dishes", JSON.stringify(this.store.cart));
           return;
         }
       }
@@ -117,94 +120,101 @@ export default {
 
 <template>
   <main>
-    <div class="cart-position m-5">
-      <AppCart class="position-absolute" />
-    </div>
-
-    <div class="container my-5">
-      <div class="info-restaurant d-flex flex-row-reverse">
-        <div class="left-img m-2" v-if="restaurant">
-          <img
-            :src="`${store.api.baseUrl}/storage/${restaurant.restaurant.img}`"
-            alt="piatto"
-          />
-        </div>
-
-        <div class="caption d-flex flex-column justify-content-between">
-          <h1 v-if="restaurant">{{ restaurant.restaurant.name_restaurant }}</h1>
-          <div>
-            <h5 v-if="restaurant">{{ restaurant.restaurant.address }}</h5>
-            <h5 v-if="restaurant">{{ restaurant.restaurant.phone_number }}</h5>
-          </div>
-        </div>
+    <AppPageLoader v-if="isLoading" />
+    <div v-else>
+      <div class="cart-position m-5">
+        <AppCart class="position-absolute" />
       </div>
 
-      <div class="menu">
-        <h3 v-if="restaurant.dishes.length === 1">É presente 1 piatto</h3>
-        <h3 v-else>Sono presenti: {{ restaurant.dishes.length }} piatti</h3>
-
-        <ul class="d-flex flex-wrap row p-0">
-          <li
-            class="col-6 col-sm-4 col-lg-3 col-xl-3 g-3 my-2"
-            v-for="dish in restaurant.dishes"
-          >
-            <AppDish
-              :dish_new="dish"
-              @newItem="addToCart(dish)"
-              @removeItem="removeToCart(dish)"
+      <div class="container my-5">
+        <div class="info-restaurant d-flex flex-row-reverse">
+          <div class="left-img m-2" v-if="restaurant">
+            <img
+              :src="`${store.api.baseUrl}/storage/${restaurant.restaurant.img}`"
+              alt="piatto"
             />
-            <!-- Modal -->
-            <div
-              class="modal bg-dark"
-              style="--bs-bg-opacity: 0.8"
-              :class="modalOn && currentModal === dish.id ? 'd-block' : ''"
-              @click.self="toogleModal(dish.id)"
+          </div>
+
+          <div class="caption d-flex flex-column justify-content-between">
+            <h1 v-if="restaurant">
+              {{ restaurant.restaurant.name_restaurant }}
+            </h1>
+            <div>
+              <h5 v-if="restaurant">{{ restaurant.restaurant.address }}</h5>
+              <h5 v-if="restaurant">
+                {{ restaurant.restaurant.phone_number }}
+              </h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="menu">
+          <h3 v-if="restaurant.dishes.length === 1">É presente 1 piatto</h3>
+          <h3 v-else>Sono presenti: {{ restaurant.dishes.length }} piatti</h3>
+
+          <ul class="d-flex flex-wrap row p-0">
+            <li
+              class="col-6 col-sm-4 col-lg-3 col-xl-3 g-3 my-2"
+              v-for="dish in restaurant.dishes"
             >
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h3 class="modal-title fs-5">
-                      Conferma svuotamento carrello
-                    </h3>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      @click="toogleModal(dish.id)"
-                    ></button>
-                  </div>
-                  <div class="modal-body">
-                    Se aggiungi un piatto di un altro ristorante il carrello
-                    verrà svuotato, vuoi procedere comunque?
-                  </div>
-                  <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-primary"
-                      @click="toogleModal(dish.id)"
-                    >
-                      Annulla
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-success"
-                      @click="addToCart(dish)"
-                    >
-                      Conferma
-                    </button>
+              <AppDish
+                :dish_new="dish"
+                @newItem="addToCart(dish)"
+                @removeItem="removeToCart(dish)"
+              />
+              <!-- Modal -->
+              <div
+                class="modal bg-dark"
+                style="--bs-bg-opacity: 0.8"
+                :class="modalOn && currentModal === dish.id ? 'd-block' : ''"
+                @click.self="toogleModal(dish.id)"
+              >
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h3 class="modal-title fs-5">
+                        Conferma svuotamento carrello
+                      </h3>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        @click="toogleModal(dish.id)"
+                      ></button>
+                    </div>
+                    <div class="modal-body">
+                      Se aggiungi un piatto di un altro ristorante il carrello
+                      verrà svuotato, vuoi procedere comunque?
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        @click="toogleModal(dish.id)"
+                      >
+                        Annulla
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-success"
+                        @click="addToCart(dish)"
+                      >
+                        Conferma
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <!-- Modal -->
-          </li>
-        </ul>
+              <!-- Modal -->
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <style scoped lang="scss">
-@import '../assets/scss/partials/variables.scss';
+@import "../assets/scss/partials/variables.scss";
 .left-img {
   width: 40%;
 
