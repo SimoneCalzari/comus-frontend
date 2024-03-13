aaf7a6ca338c2a60c48381b2d0425d49238822f8
elisabichiappini <elisabichiappini@gmail.com>
2024-03-13 15:49:19 +0100

    feat: downlaod

--- src/components/AppDownload.vue
+++ src/components/AppDownload.vue
@@ -10,14 +10,14 @@ export default {
 <template>
   <!-- section download -->
   <section id="download">
-      <div class=" d-flex flex-sm-column flex-md-row g-0 container-md justify-content-center align-items-center text-center">
+      <div class="box-download d-flex flex-sm-column flex-md-row container-md justify-content-center align-items-center text-center">
         <!-- scarica testo e apps -->
         <!-- illustrazione scarica -->
         <div class="phone col-md-6 col-sm-12 order-md-first order-sm-last">
-          <img src="/img/phone-download.png" class="phone-download" />
+          <img src="/img/phone-download.png" class="phone-download"/>
         </div>
         <!-- illustrazione scarica -->
-        <div class="col-md-6 col-sm-12 order-sm-first order-md-last d-flex justify-content-center flex-column"
+        <div class="card-download col-md-6 col-sm-12 order-sm-first order-md-last d-flex justify-content-center flex-column"
         >
           <h2>Scarica l'app</h2>
           <p>
@@ -26,14 +26,14 @@ export default {
           </p>
 
           <div class="d-flex gap-2 justify-content-center">
-            <a href="#" target="_blank">
+            <a href="#">
               <img
                 src="/img/app-store.png"
                 alt="app-store"
                 class="custom-app"
               />
             </a>
-            <a href="#" target="_blank">
+            <a href="#">
               <img
                 src="/img/play-store.svg"
                 alt="play-store"
@@ -55,51 +55,62 @@ export default {
 
 #download {
   background-color: $custom-light;
-  padding: $size-48 0;
+  background-image: url('/img/table.jpg');
+  background-size: cover;
 
-  h2 {
-    font-family: 'Bevan', serif;
-  }
+  .box-download {
+    padding: $size_40;
+    gap: $size_32;
 
-  .custom-app {
-    width: 120px;
-    height: 40px;
-    object-fit: cover;
-    border-radius: 10px;
-  }
+    .card-download {
+      padding: $size_8;
+      overflow: hidden;
+      height: 100%;
+      border-radius: $size_32;
+      border: 1px solid $custom-light;
+      background-color: $custom-white;
+    }
+
+    .custom-app {
+      width: 120px;
+      height: 40px;
+      object-fit: cover;
+      border-radius: 10px;
+    }
 
   .phone {
     padding: $size_48 0;
-    img {
-
+    .phone-download {
       object-fit: cover;
       margin-top: $size_16;
       margin: 0;
       transform: rotate(20deg);
     }
   }
-    // Media queries
-    @media screen and (max-width: 1200px) {
-      .phone-download {
-      width: 60%;
-    }
-  }
-  @media screen and (max-width: 992px) {
-    .phone-download {
-      width: 60%;
-    }
-  }
-  @media screen and (max-width: 768px) {
-    .phone-download {
-      width: 40%;
-    }
   }
   
-  @media screen and (max-width: 576px) {
-    .phone-download {
-      width: 30%;
-    }
-  }
+  //   // Media queries
+  //   @media screen and (max-width: 1200px) {
+  //     .phone-download {
+  //     width: 60%;
+  //   }
+  // }
+  // @media screen and (max-width: 992px) {
+  //   .phone-download {
+  //     width: 60%;
+  //   }
+  // }
+  // @media screen and (max-width: 768px) {
+  //   .phone-download {
+  //     width: 40%;
+  //   }
+  // }
+  
+  // @media screen and (max-width: 576px) {
+  //   .phone-download {
+  //     width: 30%;
+  //   }
+  // }
 }
 
 </style>
