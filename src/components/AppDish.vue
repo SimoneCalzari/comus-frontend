6332101f8c8a485c2457f8b3de99adb2b56b6e93
JohnQuimson <john.quimson777@gmail.com>
2024-03-13 15:09:16 +0100

    feat responsive restaurants

--- src/components/AppDish.vue
+++ src/components/AppDish.vue
@@ -1,21 +1,21 @@
 <script>
-import store from "../store";
+import store from '../store';
 
 export default {
-  name: "AppDish",
+  name: 'AppDish',
   data() {
     return {
       store,
     };
   },
   methods: {},
-  emits: ["newItem" , "removeItem"],
-  props: ["dish_new"],
+  emits: ['newItem', 'removeItem'],
+  props: ['dish_new'],
 };
 </script>
 
 <template>
-  <div class="menu-item p-3 d-flex position-relative h-100">
+  <!-- <div class="menu-item d-flex position-relative h-100 border">
     <div class="dish-img m-1">
       <img
         :src="`${store.api.baseUrl}/storage/${dish_new.img}`"
@@ -28,21 +28,85 @@ export default {
       <h5>{{ dish_new.price }} €</h5>
     </div>
     <div class="d-flex shrink-0 align-items-end">
-      <div type="button" class="m-1 custom-btn" @click="$emit('newItem')">+</div>
-      <div type="button" class="m-1 custom-btn" @click="$emit('removeItem')">-</div>
+      <div type="button" class="m-1 custom-btn" @click="$emit('newItem')">
+        +
+      </div>
+      <div type="button" class="m-1 custom-btn" @click="$emit('removeItem')">
+        -
+      </div>
     </div>
-  </div>
+  </div> -->
+  <router-link
+    :to="{ name: 'restaurant', params: { slug: restaurant.slug } }"
+    class="col-3 g-3"
+    v-for="restaurant in store.restaurants"
+  >
+    <div class="card-restaurant d-flex flex-column align-items-center">
+      <!-- img -->
+      <div class="cont-img">
+        <img
+          :src="`${store.api.baseUrl}/storage/${restaurant.img}`"
+          alt="immagine ristorante"
+        />
+      </div>
+      <!-- name -->
+      <div class="cont-text">
+        <h6 class="text-center">
+          {{ restaurant.name_restaurant }}
+        </h6>
+        <!-- types -->
+        <ul class="p-0">
+          <li v-for="category in restaurant.types" class="text-center">
+            <span>
+              {{ category.name_type }}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </router-link>
 </template>
 
 <style scoped lang="scss">
-@import "../assets/scss/partials/variables.scss";
+@import '../assets/scss/partials/variables.scss';
 .menu-item {
-  border: 1px solid $custom-primary;
-  border-radius: 10px 25px 10px 10px;
-  color: $custom-secondary;
-  background-color: $custom-light;
+  padding: 0;
+  overflow: hidden;
+  height: 100%;
+  // border-radius: $size_32;
+
+  .cont-img {
+    overflow: hidden;
+    //border-radius: $size_32;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      display: block;
+    }
+  }
+
+  .cont-info {
+    border-radius: 0 0 $size_32 $size_32;
+    transition: all 0.6s ease-in-out; //ovunque
+    flex-grow: 1;
+
+    h6 {
+      color: $custom-white;
+      font-family: 'Bevan', serif;
+      background-color: $custom-primary;
+      border-radius: $size_32;
+      padding: $size_8 0;
+      margin-top: $size_8;
+    }
+
+    span {
+      color: $custom-secondary;
+    }
+  }
 }
-.custom-btn{
+.custom-btn {
   bottom: 5px;
   right: 5px;
   text-decoration: none;
